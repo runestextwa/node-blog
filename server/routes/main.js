4fc4f8a9485c4f7058dd6191838ca099926857a2
runestextwa <devilstea@gmail.com>
2023-05-19 01:11:29 +0500

    Revert "adding info in a router for rendering"
    This reverts commit cc23d47f103f930fd53c78fb2db4a868806a5065.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
-    const info = {
-        name: 'nodeJS blog',
-        description: "that is just a description for testing purposes nothing to see here"
-    }
-
-    res.render('index', info);
-
+    console.log('Hello, World!');
+    res.send('<h1>Hello, World!</h1>');
 });
-
-
-
-
-
 
 module.exports = router;
